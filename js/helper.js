7d2f7cde18d91288b154f61ec651e60d97532e00
sedano <sedano23@gmail.com>
2016-05-25 21:57:31 +0200

    Added project press tip

--- js/helper.js
+++ js/helper.js
@@ -1,5 +1,5 @@
 $.getJSON('http://ipinfo.io', function (data) {
-    console.log(data)
+    // console.log(data)
     if (data.country === "IE") {
         $("#about-text").text("I'm a Robotics Engineer with experience in both technical and project management fields. I have worked for recognized Applied Robotics Research Institutes, lead projects in the food sector and did mechanical design and validation of medical devices. I'm passionate about technology and experienced in 3D CAD design.");
     }
